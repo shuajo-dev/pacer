5091504ba4e348a57d0f5ca76f3fb3177a92690b
shuajo-dev <cabatan.josh@gmail.com>
2026-06-30 22:30:56 +0800

    feat: add health chcek endpoint

--- api/routes/api.php
+++ api/routes/api.php
@@ -12,3 +12,10 @@ Route::get('/user', function (Request $request) {
 Route::post('/register', [AuthController::class, 'register']);
 
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/health', function() {
+    return response()->json([
+        'status' => 'ok',
+        'timestamp' => now(),
+    ]);
+});
