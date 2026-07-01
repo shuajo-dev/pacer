eea9883d8cedaeaf0ca5e07db9b7be54d8a3c10a
shuajo-dev <cabatan.josh@gmail.com>
2026-07-02 00:26:42 +0800

    feat: add axios and auth for react

---
+++ client/src/api/client.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const client = axios.create({
+    baseURL: '/api',
+})
+
+client.interceptors.request.use((config) => {
+    const token = localStorage.getItem('auth_token');
+    if(token){
+        config.headers.Authorization = `Bearer ${token}`;
+
+    }
+
+    return config;
+});
+
+
+export default client;
